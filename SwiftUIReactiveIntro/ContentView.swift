dcbbe61decbbb072bd58e0b55a3749fe0f4a81d1
nevercute <nevercute@icloud.com>
2019-06-20 23:00:00 +0300

    I forgot about committin during this awesome coding session

--- SwiftUIReactiveIntro/ContentView.swift
+++ SwiftUIReactiveIntro/ContentView.swift
@@ -9,8 +9,71 @@
 import SwiftUI
 
 struct ContentView : View {
+    
+    @State var firstName = ""
+    @State var lastName = ""
+    @State var users = [String]()
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            VStack {
+                VStack {
+                    Group {
+                        TextField($firstName, placeholder: Text("First Name")).padding(12)
+                        TextField($lastName, placeholder: Text("Last Name")).padding(12)
+                        }.shadow(radius: 5).background(Color.white).clipShape(RoundedRectangle(cornerRadius: 5))
+                    HStack {
+                        Button(action: {
+                            self.users.append("\(self.firstName) \(self.lastName)")
+                            self.firstName = ""
+                            self.lastName = ""
+                        }) {
+                            Group {
+                                Text("CREATE USER")
+                                    .color(.white).padding(12)
+                                
+                                }.background(firstName.count + lastName.count == 0 ? Color.blue.opacity(0.5) : Color.blue)
+                                .clipShape(RoundedRectangle(cornerRadius:12))
+                                .shadow(radius: 5)
+                            
+                            
+                        }.disabled(firstName.count + lastName.count == 0)
+                        Button(action: {
+                            self.users.removeAll()
+                        }) {
+                            Group {
+                                Text("DELETE USERS")
+                                    .color(.white).padding(12)
+                                
+                                }.background(users.count == 0 ? Color.red.opacity(0.5) : Color.red)
+                                .clipShape(RoundedRectangle(cornerRadius:12))
+                                .shadow(radius: 5)
+                            
+                        }.disabled(users.count == 0)
+                    }.padding(.trailing, 20)
+                    
+                    }.padding(12)
+                
+                
+                }.navigationBarTitle(Text("Credit Card Form"))
+                .navigationBarItems(leading: HStack {
+                    Text("First name:")
+                    Text(firstName).color(.orange)
+                    Text("Last name:")
+                    Text(lastName).color(.orange)
+                })
+                .background(Color.gray)
+            VStack (alignment: .leading) {
+                Text("All users").font(.subheadline)
+                .padding(.bottom, -10)
+                List (users.identified(by: \.self)) {
+                    Text($0)
+                }
+            }
+            .padding(.leading, 10)
+            
+            
+        }
     }
 }
 
@@ -21,3 +84,8 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
+
+
+
+
